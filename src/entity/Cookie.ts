d930534f1101ef26d32fbf8e191501b7c0ecca6c
allen3680 <allen3680@gmail.com>
2021-06-23 18:06:22 +0800

    updateCookieStatus

--- src/entity/Cookie.ts
+++ src/entity/Cookie.ts
@@ -9,7 +9,7 @@ export class Cookie extends AdvancedEntity<Cookie> {
   @Column({ length: 256, nullable: true })
   cuser: string;
 
-  @Column({ length: 1024, nullable: true })
+  @Column({ length: 65532, nullable: true })
   cookieJson: string;
 
   @Column({ length: 256, nullable: true })
