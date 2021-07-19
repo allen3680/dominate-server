d59d6083e30ee72b178227bfac0c485300eec82b
allen3680 <allen3680@gmail.com>
2021-07-19 19:47:05 +0800

    a

--- src/controller/cookie.controller.ts
+++ src/controller/cookie.controller.ts
@@ -34,7 +34,9 @@ export class CookieController {
   }
 
   @Post('uploadstring')
+  @UseInterceptors(AnyFilesInterceptor())
   async uploadstring(
+    @UploadedFiles() files: UploadFile[],
     @Body()
     body: {
       cookieJson: any;
